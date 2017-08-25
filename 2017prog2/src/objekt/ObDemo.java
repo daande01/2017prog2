a55e48ab50c2e5156338cd4c1c11f0ea4ae2950a
daande01 <danand004@5CG428HN6D.thn.local>
2017-08-25 13:28:47 +0200

    fix

--- 2017prog2/src/objekt/ObDemo.java
+++ 2017prog2/src/objekt/ObDemo.java
@@ -3,10 +3,37 @@
  */
 package objekt;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * @author danand004
  *
  */
-public class ObDemo {
+public class ObDemo extends JFrame {
 
+	public ObDemo(){
+		setLayout(new FlowLayout());
+		
+		JLabel j1 = new JLabel("hej",JLabel.CENTER);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String [] arg){
+		
+		
+	}
+	
+	
 }
